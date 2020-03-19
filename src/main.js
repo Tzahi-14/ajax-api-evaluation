74dc79bc891949acb50cd13cdca6de633d63466c
danbrud <dannybrudner@users.noreply.github.com>
2020-03-19 17:34:52 +0200

    removed [ci skip]

--- src/main.js
+++ src/main.js
@@ -1,17 +1,1 @@
-const success = function (image) {
-  $('#result').empty().append(`<img src="${image.message}" />`)
-}
-
-const error = function (err) {
-  $('#result').empty().append('<p>Breed not found - try again.</p>')
-}
-
-$('#search').on('click', function () {
-  const input = $('#breed-input').val()
-  $.ajax({
-    url: `https://dog.ceo/api/breed/${input}/images/random`,
-    method: 'GET',
-    success,
-    error
-  })
-})
+//Your code here
